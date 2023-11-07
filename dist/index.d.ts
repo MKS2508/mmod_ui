bef8929736489bbc3cd1115bd4ad3e275e0a8575
Marcos Asensio <60777608+MKS2508@users.noreply.github.com>
2023-11-07 20:56:23 +0100

    a

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,5 +1,5 @@
-import SideBar from './dist/src/components/SideBar';
-import TopBar from './dist/src/components/TopBar';
+import SideBar from './src/components/SideBar';
+import TopBar from './src/components/TopBar';
 import ICategory from './dist/src/components/models/ICategory';
 export { SideBar, TopBar, ICategory };
 //# sourceMappingURL=index.d.ts.map
